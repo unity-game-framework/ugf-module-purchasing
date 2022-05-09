61f4db802d4b02c9a4d23f2e4a43ebc8b535fd38
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-09 21:53:19 +0200

    Add PurchaseModuleUnity

---
+++ Packages/UGF.Module.Purchasing/Runtime/Unity/PurchaseModuleUnity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UGF.Application.Runtime;
+
+namespace UGF.Module.Purchasing.Runtime.Unity
+{
+    public class PurchaseModuleUnity : PurchaseModule<PurchaseModuleUnityDescription>
+    {
+        public PurchaseModuleUnity(PurchaseModuleUnityDescription description, IApplication application) : base(description, application)
+        {
+        }
+
+        protected override Task<IPurchaseTransaction> OnPurchaseAsync(string productId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Task OnConfirmAsync(string transactionId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Task<IList<string>> OnGetPendingTransactionAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Task<IDictionary<string, IPurchaseProduct>> OnGetProductsAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
