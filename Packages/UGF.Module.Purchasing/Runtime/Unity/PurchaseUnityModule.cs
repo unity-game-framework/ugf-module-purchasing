b3c4d89599834db1865a5377162fded15f53aeea
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-10 21:58:35 +0200

    Update PurchaseUnityModule.cs

--- Packages/UGF.Module.Purchasing/Runtime/Unity/PurchaseUnityModule.cs
+++ Packages/UGF.Module.Purchasing/Runtime/Unity/PurchaseUnityModule.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UGF.Application.Runtime;
-using UGF.Initialize.Runtime;
 using UGF.Logs.Runtime;
+using UGF.RuntimeTools.Runtime.Tasks;
 using UnityEngine.Purchasing;
 
 namespace UGF.Module.Purchasing.Runtime.Unity
 {
     public class PurchaseUnityModule : PurchaseModule<PurchaseUnityModuleDescription>
     {
+        public PurchaseUnityStore Store { get { return m_store ?? throw new ArgumentException("Value not specified."); } }
+
+        private readonly HashSet<string> m_pending = new HashSet<string>();
         private PurchaseUnityStore m_store;
+        private bool m_processingPurchase;
+        private bool? m_processingPurchaseResult;
 
         public PurchaseUnityModule(PurchaseUnityModuleDescription description, IApplication application) : base(description, application)
         {
@@ -30,7 +36,8 @@ namespace UGF.Module.Purchasing.Runtime.Unity
             }
 
             m_store = new PurchaseUnityStore(builder);
-            m_store.TransactionFailed += OnStoreTransactionFailed;
+            m_store.PurchasePending += OnStorePurchasePending;
+            m_store.PurchaseFailed += OnStorePurchaseFailed;
 
             Log.Debug("Purchase Unity module initialized", new
             {
@@ -41,7 +48,7 @@ namespace UGF.Module.Purchasing.Runtime.Unity
 
         protected override async Task OnInitializeAsync()
         {
-            await m_store.InitializeAsync();
+            await Store.InitializeAsync();
 
             Log.Debug("Purchase Unity module store initialize complete", new
             {
@@ -53,33 +60,80 @@ namespace UGF.Module.Purchasing.Runtime.Unity
         {
             base.OnUninitialize();
 
-            m_store.TransactionFailed -= OnStoreTransactionFailed;
+            m_store.PurchasePending -= OnStorePurchasePending;
+            m_store.PurchaseFailed -= OnStorePurchaseFailed;
             m_store = null;
         }
 
         protected override bool OnCheckAvailable()
         {
-            if (m_store == null) throw new InitializeStateException();
-
-            return m_store.IsInitialized;
+            return Store.IsInitialized;
         }
 
-        protected override Task<IPurchaseTransaction> OnPurchaseAsync(string productId)
+        protected override bool OnCheckProcessingPurchase()
         {
+            return m_processingPurchase;
         }
 
-        protected override Task OnConfirmAsync(string transactionId)
+        protected override async Task<bool> OnPurchaseStartAsync(string productId)
         {
+            Product product = Store.Controller.products.WithID(productId);
+
+            if (product == null) throw new ArgumentException($"Product not found by the specified id: '{productId}'.");
+
+            m_processingPurchase = true;
+
+            try
+            {
+                Store.Controller.InitiatePurchase(product);
+
+                while (m_processingPurchaseResult == null)
+                {
+                    await Task.Yield();
+                }
+
+                bool result = m_processingPurchaseResult.Value;
+
+                return result;
+            }
+            finally
+            {
+                m_processingPurchase = false;
+                m_processingPurchaseResult = null;
+            }
         }
 
-        protected override Task<IList<string>> OnGetPendingTransactionAsync()
+        protected override Task OnPurchaseConfirmAsync(string productId)
         {
+            if (!m_pending.Contains(productId)) throw new ArgumentException($"Pending product not found by the specified id: '{productId}'.");
+
+            Product product = Store.Controller.products.WithID(productId);
+
+            if (product == null) throw new ArgumentException($"Product not found by the specified id: '{productId}'.");
+
+            Store.Controller.ConfirmPendingPurchase(product);
+
+            m_pending.Remove(productId);
+
+            return Task.CompletedTask;
+        }
+
+        protected override Task<IList<string>> OnGetPendingProductsAsync()
+        {
+            var result = new List<string>();
+
+            foreach (string productId in m_pending)
+            {
+                result.Add(productId);
+            }
+
+            return Task.FromResult<IList<string>>(result);
         }
 
         protected override Task<IDictionary<string, IPurchaseProduct>> OnGetProductsAsync()
         {
             var result = new Dictionary<string, IPurchaseProduct>();
-            ProductCollection products = m_store.Controller.products;
+            ProductCollection products = Store.Controller.products;
 
             foreach (Product unityProduct in products.set)
             {
@@ -91,8 +145,28 @@ namespace UGF.Module.Purchasing.Runtime.Unity
             return Task.FromResult<IDictionary<string, IPurchaseProduct>>(result);
         }
 
-        private void OnStoreTransactionFailed(IPurchaseTransaction transaction)
+        protected override Task<TaskResult<string>> OnTryGetTransactionIdAsync(string productId)
         {
+            Product product = Store.Controller.products.WithID(productId);
+
+            return product != null && !string.IsNullOrEmpty(product.transactionID)
+                ? Task.FromResult<TaskResult<string>>(product.transactionID)
+                : Task.FromResult(TaskResult<string>.Empty);
+        }
+
+        private void OnStorePurchasePending(string productId)
+        {
+            m_pending.Add(productId);
+
+            if (m_processingPurchase)
+            {
+                m_processingPurchaseResult = true;
+            }
+        }
+
+        private void OnStorePurchaseFailed(string productId)
+        {
+            m_processingPurchaseResult = false;
         }
     }
 }
