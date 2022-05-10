b049e756a9de5a7fe9841ae76aed38466b830d0e
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-10 19:25:12 +0200

    Add PurchaseProductDescriptionAsset

---
+++ Packages/UGF.Module.Purchasing/Runtime/PurchaseProductDescriptionAsset.cs
@@ -0,0 +1,26 @@
+﻿using UGF.Builder.Runtime;
+using UnityEngine;
+
+namespace UGF.Module.Purchasing.Runtime
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Purchasing/Purchase Product Description", order = 2000)]
+    public class PurchaseProductDescriptionAsset : BuilderAsset<PurchaseProductDescription>
+    {
+        [SerializeField] private string m_storeId;
+        [SerializeField] private PurchaseProductType m_productType = PurchaseProductType.Consumable;
+
+        public string StoreId { get { return m_storeId; } set { m_storeId = value; } }
+        public PurchaseProductType ProductType { get { return m_productType; } set { m_productType = value; } }
+
+        protected override PurchaseProductDescription OnBuild()
+        {
+            var description = new PurchaseProductDescription
+            {
+                StoreId = m_storeId,
+                StoreType = m_productType
+            };
+
+            return description;
+        }
+    }
+}
