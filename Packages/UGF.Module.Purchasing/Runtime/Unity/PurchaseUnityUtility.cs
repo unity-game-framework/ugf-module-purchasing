5ff40fd779560044f8a95ad98ab2253c769e9ec5
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-10 20:15:08 +0200

    Update PurchaseUnityUtility.cs

--- Packages/UGF.Module.Purchasing/Runtime/Unity/PurchaseUnityUtility.cs
+++ Packages/UGF.Module.Purchasing/Runtime/Unity/PurchaseUnityUtility.cs
@@ -5,33 +5,46 @@ namespace UGF.Module.Purchasing.Runtime.Unity
 {
     public static class PurchaseUnityUtility
     {
+        public static PurchaseProduct GetProduct(Product unityProduct)
+        {
+            var price = new PurchasePrice((float)unityProduct.metadata.localizedPrice, unityProduct.metadata.localizedPriceString, unityProduct.metadata.isoCurrencyCode);
+            var product = new PurchaseProduct(price);
+
+            if (unityProduct.hasReceipt)
+            {
+                product.SetReceipt(unityProduct.receipt);
+            }
+
+            return product;
+        }
+
         public static ProductType GetProductType(PurchaseProductType productType)
         {
             return TryGetProductType(productType, out ProductType value) ? value : throw new ArgumentException($"Product type not found by the specified type: '{productType}'.");
         }
 
-        public static bool TryGetProductType(PurchaseProductType productType, out ProductType value)
+        public static bool TryGetProductType(PurchaseProductType productType, out ProductType unityProductType)
         {
             switch (productType)
             {
                 case PurchaseProductType.Consumable:
                 {
-                    value = ProductType.Consumable;
+                    unityProductType = ProductType.Consumable;
                     return true;
                 }
                 case PurchaseProductType.NonConsumable:
                 {
-                    value = ProductType.NonConsumable;
+                    unityProductType = ProductType.NonConsumable;
                     return true;
                 }
                 case PurchaseProductType.Subscription:
                 {
-                    value = ProductType.Subscription;
+                    unityProductType = ProductType.Subscription;
                     return true;
                 }
             }
 
-            value = default;
+            unityProductType = default;
             return false;
         }
     }
