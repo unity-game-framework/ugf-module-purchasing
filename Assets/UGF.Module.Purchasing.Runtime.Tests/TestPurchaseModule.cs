5d60a5298e915c0d4fb721899d36e48a3b555029
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-12 20:09:08 +0200

    Update TestPurchaseModule.cs

--- Assets/UGF.Module.Purchasing.Runtime.Tests/TestPurchaseModule.cs
+++ Assets/UGF.Module.Purchasing.Runtime.Tests/TestPurchaseModule.cs
@@ -15,7 +15,7 @@ namespace UGF.Module.Purchasing.Runtime.Tests
 
             var module = launcher.Application.GetModule<IPurchaseModule>();
 
-            bool result = await module.PurchaseAsync("");
+            bool result = await module.PurchaseAsync("047297d90e59e2c4c848667d9968f8a0");
 
             Assert.True(result);
 
