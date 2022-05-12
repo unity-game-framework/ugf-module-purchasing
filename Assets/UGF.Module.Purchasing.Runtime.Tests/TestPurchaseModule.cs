71e28d7b58a9cb64f5807ff5096e994a10f298c9
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-12 20:01:58 +0200

    Add tests

---
+++ Assets/UGF.Module.Purchasing.Runtime.Tests/TestPurchaseModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using UGF.Application.Runtime;
+
+namespace UGF.Module.Purchasing.Runtime.Tests
+{
+    public class TestPurchaseModule
+    {
+        [Test]
+        public async Task PurchaseAsync()
+        {
+            var launcher = new ApplicationLauncherResources("Launcher");
+
+            await launcher.CreateAsync();
+
+            var module = launcher.Application.GetModule<IPurchaseModule>();
+
+            bool result = await module.PurchaseAsync("");
+
+            Assert.True(result);
+
+            launcher.Destroy();
+        }
+    }
+}
