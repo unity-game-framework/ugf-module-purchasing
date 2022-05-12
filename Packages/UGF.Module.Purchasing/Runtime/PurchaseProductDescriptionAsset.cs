d5e406c24b3674c9379c8e90df5e93a293b46ca3
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-12 20:00:20 +0200

    Update PurchaseProductDescriptionAsset.cs

--- Packages/UGF.Module.Purchasing/Runtime/PurchaseProductDescriptionAsset.cs
+++ Packages/UGF.Module.Purchasing/Runtime/PurchaseProductDescriptionAsset.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace UGF.Module.Purchasing.Runtime
 {
     [CreateAssetMenu(menuName = "Unity Game Framework/Purchasing/Purchase Product Description", order = 2000)]
-    public class PurchaseProductDescriptionAsset : BuilderAsset<PurchaseProductDescription>
+    public class PurchaseProductDescriptionAsset : BuilderAsset<IPurchaseProductDescription>
     {
         [SerializeField] private string m_id;
         [SerializeField] private PurchaseProductType m_type = PurchaseProductType.Consumable;
@@ -12,7 +12,7 @@ namespace UGF.Module.Purchasing.Runtime
         public string ID { get { return m_id; } set { m_id = value; } }
         public PurchaseProductType Type { get { return m_type; } set { m_type = value; } }
 
-        protected override PurchaseProductDescription OnBuild()
+        protected override IPurchaseProductDescription OnBuild()
         {
             var description = new PurchaseProductDescription
             {
